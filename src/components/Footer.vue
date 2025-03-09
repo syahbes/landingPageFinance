924927f576b923c76be49f6d0f056ba5f58c0e80
Shlomi Yahbes <syahbes@gmail.com>
2025-03-09 21:56:06 +0200

    footer desktop

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -33,67 +33,80 @@ const legalLinks = [
 
 <template>
   <footer>
-    <div class="logos-container">
-      <img
-        alt="Finance Magnates logo"
-        src="@/assets/logo.svg"
-        class="logo-primary"
-      />
-      <img
-        alt="Finance Magnates logo"
-        src="@/assets/union.png"
-        class="logo-secondary"
-      />
-    </div>
-    <div class="footer-info-container">
-      <div class="text-and-social">
-        <div class="text">
-          <p>
-            Finance Magnates Intelligence, part of Finance Magnates, delivers
-            specialized research and insights on the financial markets. Offering
-            Quarterly, Annual, and custom reports tailored to specific business
-            needs, it covers key aspects such as trading volumes, market trends,
-            regulations, and more
-          </p>
-        </div>
-        <div class="social-links">
-          <a
-            v-for="(link, index) in socialLinks"
-            :key="index"
-            :href="link.url"
-            :aria-label="link.ariaLabel"
-            class="social-link"
-          >
-            <img :src="link.icon" :alt="link.ariaLabel" class="social-icon" />
-          </a>
-        </div>
+    <div class="footer-top">
+      <div class="logos-container">
+        <img
+          alt="Finance Magnates logo"
+          src="@/assets/logo.svg"
+          class="logo-primary"
+        />
+        <img
+          alt="Finance Magnates logo"
+          src="@/assets/union.png"
+          class="logo-secondary"
+        />
       </div>
-      <nav class="footer-nav">
-        <ul>
-          <li v-for="link in navLinks" :key="link.text">
-            <a :href="link.url">{{ link.text }}</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-    <div class="footer-bottom">
-      <div class="copyright">
-        <p>© 2024 "Finance Magnates CY Ltd." All rights reserved.</p>
+      <div class="footer-info-container">
+        <div class="text-and-social">
+          <div class="text">
+            <p>
+              Finance Magnates Intelligence, part of Finance Magnates, delivers
+              specialized research and insights on the financial markets.
+              Offering Quarterly, Annual, and custom reports tailored to
+              specific business needs, it covers key aspects such as trading
+              volumes, market trends, regulations, and more
+            </p>
+          </div>
+          <div class="social-links">
+            <a
+              v-for="(link, index) in socialLinks"
+              :key="index"
+              :href="link.url"
+              :aria-label="link.ariaLabel"
+              class="social-link"
+            >
+              <img :src="link.icon" :alt="link.ariaLabel" class="social-icon" />
+            </a>
+          </div>
+        </div>
+        <nav class="footer-nav">
+          <ul>
+            <li v-for="link in navLinks" :key="link.text">
+              <a :href="link.url">{{ link.text }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+    </div>
 
-      <div class="legal-links">
-        <ul>
-          <li v-for="(link, index) in legalLinks" :key="index">
-            <a :href="link.url">{{ link.text }}</a>
-            <span v-if="index < legalLinks.length - 1" class="separator"></span>
-          </li>
-        </ul>
+    <div>
+      <div class="footer-bottom">
+        <div class="copyright">
+          <p>© 2025 "Finance Magnates CY Ltd." All rights reserved.</p>
+        </div>
+
+        <div class="legal-links">
+          <ul>
+            <li v-for="(link, index) in legalLinks" :key="index">
+              <a :href="link.url">{{ link.text }}</a>
+              <span
+                v-if="index < legalLinks.length - 1"
+                class="separator"
+              ></span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
 <style>
+p {
+  margin: 0;
+  padding: 0;
+  color: var(--color-gray);
+}
 .social-link {
   display: inline-block;
   margin-right: 10px;
@@ -103,22 +116,75 @@ const legalLinks = [
     scale: 1.1;
   }
 }
-.footer-nav ul {
+.footer-nav ul,
+.legal-links ul {
   list-style: none;
   padding: 0;
   margin: 0;
 }
 
 .footer-nav li {
-  margin-bottom: 0.75rem;
+  margin-bottom: 5px;
+  width: 200px;
 }
 
-.footer-nav a {
-  color: #fff;
+.footer-nav a,
+.legal-links a {
+  color: var(--color-gray);
   text-decoration: none;
+  /* hover */
+  &:hover {
+    color: aliceblue;
+  }
 }
 
-@media (max-width: 922px) {
+.footer-top {
+  max-width: 1440px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: start;
+  gap: 50px;
+  padding: 20px 10px;
+}
+
+.logos-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  gap: 20px;
+}
+.footer-info-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 20px;
+}
+.text-and-social {
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+}
+.footer-bottom {
+  max-width: 1440px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 30px 0;
+}
+
+.legal-links ul {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+
+/* @media (max-width: 922px) {
   .logos-container {
     max-width: 640px;
     width: 100%;
@@ -143,5 +209,5 @@ const legalLinks = [
     justify-content: space-between;
     align-items: center;
   }
-}
+} */
 </style>
